ba9808dbf93aebacdce46e82bf9e5f74e248fbf8
ks <kskulikovaa@gmail.com>
2026-01-08 16:46:15 +0100

    add docker-compose; upd hello message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World! How y'all doing?</h1>')
 })
 
 const PORT = 8080
